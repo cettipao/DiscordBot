02421f631295ae22f56e30f5a47a2aaa676e63f4
cettipao <cettipao@live.com.ar>
2020-06-03 13:53:50 -0300

    Comando Ping y Token

--- src/index.py
+++ src/index.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
 
-commands.Bot()
-print("Primer commit desde VSC")
+bot = commands.Bot(command_prefix=">",description = "El primer Bot de CettiPao")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+
+
+bot.run('NzE3NzY3ODgzMTQ4NDkyOTMw.XtfJhw.Edlzp5w1dliDdsJh6OroyOELAwo')
